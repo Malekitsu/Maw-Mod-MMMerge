e4e475d95a4cc9d3576fc4301a49542102ae7a9c
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-07-14 02:26:16 +0200

    Update zzMaw-Spells.lua

--- Scripts/General/zzMaw-Spells.lua
+++ Scripts/General/zzMaw-Spells.lua
@@ -1,13 +1,9 @@
---NERF TO DOP AND DOG
+-------------------------------------------------
+--Nerf to Day of Protection and Day of the Gods--
+-------------------------------------------------
 function events.Tick()
-	if Party.SpellBuffs[2].Skill>=2 then
-		m=Party.SpellBuffs[2].Skill
-		Party.SpellBuffs[2].Power=Party.SpellBuffs[2].Power/(m+1)*(m/2)
-		Party.SpellBuffs[2].Skill=1
-	end
-
+	--Day of Protection
 	dopList = {0, 1, 4, 6, 12, 17}
-
 	for i = 1, #dopList do
 		item=dopList[i]
 		if Party.SpellBuffs[item].Skill>=2 then
@@ -16,4 +12,27 @@ function events.Tick()
 			Party.SpellBuffs[item].Skill=1
 		end
 	end
+	
+	--Day of the Gods
+	if Party.SpellBuffs[2].Skill>=2 then
+		m=Party.SpellBuffs[2].Skill
+		Party.SpellBuffs[2].Power=Party.SpellBuffs[2].Power/(m+1)*(m/2)+5*m
+		Party.SpellBuffs[2].Skill=1
+	end
 end
+
+----------------
+--Tooltips fix--
+----------------
+
+--Day of Protection
+Game.SpellsTxt[83].Description="Temporarily increases all seven stats on all your characters by 1 per skill in Light Magic.  This spell lasts until you rest."
+Game.SpellsTxt[83].Expert="All stats increased by 10+1 per skill"
+Game.SpellsTxt[83].Master="All stats increased by 15+1.5 per skill"
+Game.SpellsTxt[83].GM="All stats increased by 20+2 per skill"
+
+--Day of the Gods
+Game.SpellsTxt[85].Description
+  "Simultaneously casts Protection from Fire, Air, Water, Earth, Mind, and Body, plus Feather Fall and Wizard Eye on all your characters at two times your skill in Light Magic."
+Game.SpellsTxt[85].Master="All spells cast at two times skill"
+Game.SpellsTxt[85].GM="All spells cast at three times skill"
