0c5cb498e3543587e28ffd2627adc1e72e45be4b
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-08-01 04:04:35 +0200

    stats effect halved

--- Scripts/General/zzMaw-Stats.lua
+++ Scripts/General/zzMaw-Stats.lua
@@ -6,15 +6,15 @@ function events.CalcDamageToMonster(t)
 		if data.Object==nil or data.Object.Spell==133 then
 			luck=data.Player:GetLuck()
 			accuracy=data.Player:GetAccuracy()
-			critDamage=accuracy/250
-			critChance=50+luck
+			critDamage=accuracy/500
+			critChance=50+luck/2
 			roll=math.random(1, 1000)
 			if roll <= critChance then
 				t.Result=t.Result*(1.5+critDamage)
 			end
 	--might bonus
 			might=data.Player:GetMight()
-			damageBonus=might/500
+			damageBonus=might/1000
 			t.Result=t.Result*(1+damageBonus)
 		end
 	end
@@ -23,7 +23,7 @@ end
 --speed
 function events.CalcDamageToPlayer(t)
 	speed=t.Player:GetSpeed()
-	speedEffect=speed/5
+	speedEffect=speed/10
 	dodgeChance=(1-0.995^speedEffect)*100
 	roll=math.random(1, 100)
 	if roll<=dodgeChance then
@@ -41,8 +41,8 @@ function events.CalcSpellDamage(t)
 		personality=data.Player:GetPersonality()
 		luck=data.Player:GetLuck()
 		bonus=math.max(intellect,personality)
-		critDamage=bonus/500
-		t.Result=t.Result*(1+bonus/500)
+		critDamage=bonus/1000
+		t.Result=t.Result*(1+bonus/1000)
 		critChance=50+luck
 		roll=math.random(1, 1000)
 		if roll <= critChance then
@@ -54,7 +54,7 @@ end
 function events.CalcDamageToPlayer(t)
 	if t.DamageKind==4 then 
 	AC=t.Player:GetArmorClass()
-	t.Result=t.Result/(math.max(AC^0.85/100+0.5,1))
+	t.Result=t.Result/(math.max(AC^0.85/200+0.75,1))
 	end
 end
 --endurance
@@ -65,7 +65,7 @@ end
 
 function events.CalcStatBonusByItems(t)
   if t.Stat == const.Stats.HP then
-	endurance=t.Player:GetEndurance()/500
+	endurance=t.Player:GetEndurance()/1000
 	i=t.Player:GetIndex()
 	t.Result=t.Result+fullHP[i]*endurance
   end
@@ -81,7 +81,7 @@ function events.BuildStatInformationBox(t)
 	if t.Stat==0 then
 	i=Game.CurrentPlayer
 	might=Party[i]:GetMight()
-	t.Text=string.format("%s\n\nBonus Meele/Bow Damage: %s%s",Game.StatsDescriptions[0],might/5,"%")
+	t.Text=string.format("%s\n\nBonus Meele/Bow Damage: %s%s",Game.StatsDescriptions[0],might/10,"%")
 	end
 	if t.Stat==1 then
 	i=Game.CurrentPlayer
@@ -89,35 +89,35 @@ function events.BuildStatInformationBox(t)
 	fullSP=Party[i]:GetFullSP()
 	personality=Party[i]:GetPersonality()
 	intellect=Party[i]:GetIntellect()
-	t.Text=string.format("%s\n\nBonus magic damage/healing: %s%s\n\nCritical spell strike damage/healing: %s%s",Game.StatsDescriptions[1],intellect/5,"%",intellect/5+50,"%")
+	t.Text=string.format("%s\n\nBonus magic damage/healing: %s%s\n\nCritical spell strike damage/healing: %s%s",Game.StatsDescriptions[1],intellect/10,"%",intellect/10+50,"%")
 	end
 	if t.Stat==2 then
 	i=Game.CurrentPlayer
 	personality=Party[i]:GetPersonality()
-	t.Text=string.format("%s\n\nBonus magic damage/healing: %s%s\n\nCritical spell strike damage/healing bonus: %s%s",Game.StatsDescriptions[2],personality/5,"%",personality/5+50,"%")
+	t.Text=string.format("%s\n\nBonus magic damage/healing: %s%s\n\nCritical spell strike damage/healing bonus: %s%s",Game.StatsDescriptions[2],personality/10,"%",personality/10+50,"%")
 	end
 	if t.Stat==3 then
 	i=Game.CurrentPlayer
 	endurance=Party[i]:GetEndurance()
 	HPScaling=Game.Classes.HPFactor[Party[i].Class]
 	level=Party[i]:GetLevel()
-	t.Text=string.format("%s\n\nHealth bonus from Endurance: %s%s\n\nFlat HP bonus from Endurance: %s",Game.StatsDescriptions[3],endurance/5,"%",math.floor(endurance/5)*HPScaling)
+	t.Text=string.format("%s\n\nHealth bonus from Endurance: %s%s\n\nFlat HP bonus from Endurance: %s",Game.StatsDescriptions[3],endurance/10,"%",math.floor(endurance/5)*HPScaling)
 	end
 	if t.Stat==4 then
 	i=Game.CurrentPlayer
 	accuracy=Party[i]:GetAccuracy()
-	t.Text=string.format("%s\n\nCritical melee and bow strike damage bonus: %s%s",Game.StatsDescriptions[4],accuracy/2.5+50,"%")
+	t.Text=string.format("%s\n\nCritical melee and bow strike damage bonus: %s%s",Game.StatsDescriptions[4],accuracy/5+50,"%")
 	end
 	if t.Stat==5 then
 	i=Game.CurrentPlayer
 	speed=Party[i]:GetSpeed()
 	ac=Party[i]:GetArmorClass()
-	t.Text=string.format("%s\n\nDodge chance: %s%s",Game.StatsDescriptions[5],math.floor(1000-0.995^(speed/5)*1000)/10,"%")
+	t.Text=string.format("%s\n\nDodge chance: %s%s",Game.StatsDescriptions[5],math.floor(1000-0.995^(speed/10)*1000)/10	,"%")
 	end
 	if t.Stat==6 then
 	i=Game.CurrentPlayer
 	luck=Party[i]:GetLuck()
-	t.Text=string.format("%s\n\nCritical strike chance: %s%s",Game.StatsDescriptions[6],luck/10+5,"%")
+	t.Text=string.format("%s\n\nCritical strike chance: %s%s",Game.StatsDescriptions[6],luck/20+5,"%")
 	end
 	if t.Stat==7 then
 	i=Game.CurrentPlayer
@@ -130,7 +130,7 @@ function events.BuildStatInformationBox(t)
 	end
 	BBHP=HPScaling*s*m
 	local fullHP=Party[i]:GetFullHP()
-	enduranceTotalBonus=math.floor(fullHP-fullHP/(1+endurance2/500))+math.floor(endurance2/5)*HPScaling
+	enduranceTotalBonus=math.floor(fullHP-fullHP/(1+endurance2/500))+math.floor(endurance2/10)*HPScaling
 	level=Party[i]:GetLevel()
 	BASEHP=Game.Classes.HPBase[math.floor(Party[i].Class/3)]+level*HPScaling
 	t.Text=string.format("%s\n\nHP bonus from Endurance: %s\n\nHP bonus from Body building: %s\n\nHP bonus from items: %s\n\nBase HP: %s",t.Text,StrColor(0,255,0,enduranceTotalBonus), StrColor(0,255,0,BBHP),StrColor(0,255,0,math.round(fullHP-enduranceTotalBonus-BBHP-BASEHP)),StrColor(0,255,0,BASEHP))
