61cbed9bfc2bb1fcdcf327932c6d802038dcab0b
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-08-10 19:17:45 +0200

    Update zz_Maw-initialize.lua

--- Scripts/General/zz_Maw-initialize.lua
+++ Scripts/General/zz_Maw-initialize.lua
@@ -2,7 +2,7 @@
 --set true to homing projectiles for both players and monsters in real time combat
 RealTimeHoming=false
 --reworked items with multiple enchants and increased damage/armor
-ItemRework=false
+ItemRework=true
 --reworked stats, with % increase to damage etc.
-StatsRework=false
-ColouredStats=false
+StatsRework=true
+ColouredStats=true
