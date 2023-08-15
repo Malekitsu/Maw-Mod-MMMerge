5a42ee5b7402fc54af91c6b34ac4899c5be402d6
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-08-15 15:12:11 +0200

    converted into table

--- Scripts/General/zzMaw-Maps.lua
+++ Scripts/General/zzMaw-Maps.lua
@@ -1,107 +1,71 @@
 ------------------------------
---RESTORE MM6 sprites
+-- RESTORE MM6 sprites
 ------------------------------
-function events.AfterLoadMap()	
-	if Map.MapStatsIndex>=137 and Map.MapStatsIndex<=203 then
-		for i=0, Map.Sprites.High do
-			--ROCKS
-			if Map.Sprites[i].DecName=="rock01" then
-				Map.Sprites[i].DecName="Rok1"
-			elseif Map.Sprites[i].DecName=="rock02" then
-				Map.Sprites[i].DecName="Rok2"
-			elseif Map.Sprites[i].DecName=="rock03" then
-				Map.Sprites[i].DecName="Rok3"
-			elseif Map.Sprites[i].DecName=="rock04" then
-				Map.Sprites[i].DecName="Rok4"
-			elseif Map.Sprites[i].DecName=="rock05" then
-				Map.Sprites[i].DecName="Rok5"
-			elseif Map.Sprites[i].DecName=="rock06" then
-				Map.Sprites[i].DecName="Rok6"
-			elseif Map.Sprites[i].DecName=="rock07" then
-				Map.Sprites[i].DecName="Rok7"
-			elseif Map.Sprites[i].DecName=="rock08" then
-				Map.Sprites[i].DecName="Rok8"
-			elseif Map.Sprites[i].DecName=="rock09" then
-				Map.Sprites[i].DecName="Rok9"
-			elseif Map.Sprites[i].DecName=="rock10" then
-				Map.Sprites[i].DecName="Rok1"
-			elseif Map.Sprites[i].DecName=="rock11" then
-				Map.Sprites[i].DecName="Rok1"
-			elseif Map.Sprites[i].DecName=="rock12" then
-				Map.Sprites[i].DecName="Rok1"
-			elseif Map.Sprites[i].DecName=="rock13" then
-				Map.Sprites[i].DecName="Rok1"
-			elseif Map.Sprites[i].DecName=="rock14" then
-				Map.Sprites[i].DecName="Rok1"
-			--FLOWERS
-			elseif Map.Sprites[i].DecName=="flower01" then
-				Map.Sprites[i].DecName="6Flower01"
-			elseif Map.Sprites[i].DecName=="flower02" then
-				Map.Sprites[i].DecName="6Flower02"
-			elseif Map.Sprites[i].DecName=="flower03" then
-				Map.Sprites[i].DecName="6Flower03"
-			elseif Map.Sprites[i].DecName=="flower04" then
-				Map.Sprites[i].DecName="6Flower04"
-			elseif Map.Sprites[i].DecName=="flower05" then
-				Map.Sprites[i].DecName="6Flower05"
-			elseif Map.Sprites[i].DecName=="flower06" then
-				Map.Sprites[i].DecName="6Flower06"
-			elseif Map.Sprites[i].DecName=="flower07" then
-				Map.Sprites[i].DecName="6Flower07"
-			elseif Map.Sprites[i].DecName=="flower08" then
-				Map.Sprites[i].DecName="6Flower08"
-			elseif Map.Sprites[i].DecName=="flower09" then
-				Map.Sprites[i].DecName="6Flower09"
-			elseif Map.Sprites[i].DecName=="flower10" then
-				Map.Sprites[i].DecName="6Flower10"
-			elseif Map.Sprites[i].DecName=="flower11" then
-				Map.Sprites[i].DecName="6Flower11"
-			elseif Map.Sprites[i].DecName=="flower12" then
-				Map.Sprites[i].DecName="6Flower12"
-			elseif Map.Sprites[i].DecName=="flower13" then
-				Map.Sprites[i].DecName="6Flower13"
-			--CORPSES
-			elseif Map.Sprites[i].DecName=="Corpse" then
-				Map.Sprites[i].DecName="Corpse01"
-			elseif Map.Sprites[i].DecName=="Corpse01" then
-				Map.Sprites[i].DecName="Corpse02"
-			elseif Map.Sprites[i].DecName=="Corpse02" then
-				Map.Sprites[i].DecName="Corpse03"
-			elseif Map.Sprites[i].DecName=="Corpse03" then
-				Map.Sprites[i].DecName="Corpse04"
-			elseif Map.Sprites[i].DecName=="Corpse04" then
-				Map.Sprites[i].DecName="Corpse05"
-			elseif Map.Sprites[i].DecName=="Corpse05" then
-				Map.Sprites[i].DecName="Corpse06"
-			elseif Map.Sprites[i].DecName=="Corpse06" then
-				Map.Sprites[i].DecName="Corpse07"
-			elseif Map.Sprites[i].DecName=="Corpse07" then
-				Map.Sprites[i].DecName="Corpse08"
-			elseif Map.Sprites[i].DecName=="Corpse08" then
-				Map.Sprites[i].DecName="Corpse09"
-			elseif Map.Sprites[i].DecName=="Corpse09" then
-				Map.Sprites[i].DecName="Corpse10"
-			elseif Map.Sprites[i].DecName=="Corpse10" then
-				Map.Sprites[i].DecName="Corpse11"
-			elseif Map.Sprites[i].DecName=="Corpse11" then
-				Map.Sprites[i].DecName="Corpse12"
-			elseif Map.Sprites[i].DecName=="Corpse12" then
-				Map.Sprites[i].DecName="Corpse13"
-			elseif Map.Sprites[i].DecName=="Corpse13" then
-				Map.Sprites[i].DecName="Corpse14"
-			elseif Map.Sprites[i].DecName=="Corpse14" then
-				Map.Sprites[i].DecName="Corpse15"
-			elseif Map.Sprites[i].DecName=="Corpse15" then
-				Map.Sprites[i].DecName="Corpse16"
-			elseif Map.Sprites[i].DecName=="Corpse16" then
-				Map.Sprites[i].DecName="Corpse17"
-			elseif Map.Sprites[i].DecName=="Corpse17" then
-				Map.Sprites[i].DecName="Corpse18"
-			elseif Map.Sprites[i].DecName=="Corpse18" then
-				Map.Sprites[i].DecName="Corpse19"
-			elseif Map.Sprites[i].DecName=="Corpse19" then
-				Map.Sprites[i].DecName="Corpse20"
-			end
-		end
-	end
+
+-- Mapping of original sprite names to new sprite names
+local spriteMappings = {
+    -- ROCKS
+    ["rock01"] = "Rok1",
+    ["rock02"] = "Rok2",
+    ["rock03"] = "Rok3",
+    ["rock04"] = "Rok4",
+    ["rock05"] = "Rok5",
+    ["rock06"] = "Rok6",
+    ["rock07"] = "Rok7",
+    ["rock08"] = "Rok8",
+    ["rock09"] = "Rok9",
+    ["rock10"] = "Rok1",
+    ["rock11"] = "Rok1",
+    ["rock12"] = "Rok1",
+    ["rock13"] = "Rok1",
+    ["rock14"] = "Rok1",
+    
+    -- FLOWERS
+    ["flower01"] = "6Flower01",
+    ["flower02"] = "6Flower02",
+    ["flower03"] = "6Flower03",
+    ["flower04"] = "6Flower04",
+    ["flower05"] = "6Flower05",
+    ["flower06"] = "6Flower06",
+    ["flower07"] = "6Flower07",
+    ["flower08"] = "6Flower08",
+    ["flower09"] = "6Flower09",
+    ["flower10"] = "6Flower10",
+    ["flower11"] = "6Flower11",
+    ["flower12"] = "6Flower12",
+    ["flower13"] = "6Flower13",
+    
+    -- CORPSES
+    ["Corpse"] = "Corpse01",
+    ["Corpse01"] = "Corpse02",
+    ["Corpse02"] = "Corpse03",
+    ["Corpse03"] = "Corpse04",
+    ["Corpse04"] = "Corpse05",
+    ["Corpse05"] = "Corpse06",
+    ["Corpse06"] = "Corpse07",
+    ["Corpse07"] = "Corpse08",
+    ["Corpse08"] = "Corpse09",
+    ["Corpse09"] = "Corpse10",
+    ["Corpse10"] = "Corpse11",
+    ["Corpse11"] = "Corpse12",
+    ["Corpse12"] = "Corpse13",
+    ["Corpse13"] = "Corpse14",
+    ["Corpse14"] = "Corpse15",
+    ["Corpse15"] = "Corpse16",
+    ["Corpse16"] = "Corpse17",
+    ["Corpse17"] = "Corpse18",
+    ["Corpse18"] = "Corpse19",
+    ["Corpse19"] = "Corpse20",
+}
+
+function events.AfterLoadMap()
+    if Map.MapStatsIndex >= 137 and Map.MapStatsIndex <= 203 then
+        for i = 0, Map.Sprites.High do
+            local sprite = Map.Sprites[i]
+            local newSpriteName = spriteMappings[sprite.DecName]
+            if newSpriteName then
+                sprite.DecName = newSpriteName
+            end
+        end
+    end
 end
