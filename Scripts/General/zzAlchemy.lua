6bc648d82b3c780bee69e66ea7eee013bfd71c7c
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-08-30 03:31:09 +0200

    alchemy rework part 2

--- Scripts/General/zzAlchemy.lua
+++ Scripts/General/zzAlchemy.lua
@@ -51,6 +51,25 @@ function events.UseMouseItem(t)
 			end
 			return
 		end
+		
+		-------------------------
+		--PERMANENT BUFFS
+		------------------------
+		if it.Number==264 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.LuckBase=pl.LuckBase-20
+		elseif it.Number==265 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.SpeedBase=pl.SpeedBase-20
+		elseif it.Number==266 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.IntellectBase=pl.IntellectBase-20
+		elseif it.Number==267 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.EnduranceBase=pl.EnduranceBase-20
+		elseif it.Number==268 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.PersonalityBase=pl.PersonalityBase-20
+		elseif it.Number==269 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.AccuracyBase=pl.AccuracyBase-20
+		elseif it.Number==270 and not Party[0].UsedBlackPotions[it.Number] then
+			pl.MightBase=pl.MightBase-20
+		end		
 	end
 end
 
@@ -83,3 +102,67 @@ itemImmunityMapping = {
 	[251] = "paralysis",
 	[262] = "stone",
 }
+
+itemPermanentBuffMapping = {
+	[264] = "weakness",
+	[225] = "disease",
+	[226] = "poison",
+	[227] = "sleep",
+	[237] = "fear",
+	[238] = "curse",
+	[239] = "insanity",
+	[251] = "paralysis",
+	[262] = "stone",
+}
+
+function events.DoBadThingToPlayer(t)
+	if t.Allow==true then
+		if t.Thing==1 then
+			if vars.PlayerBuff[t.PlayerIndex]["curse"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Curse Immunity")
+			end
+		elseif t.Thing==2 then
+			if vars.PlayerBuff[t.PlayerIndex]["weakness"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Weakness Immunity")
+			end 
+		elseif t.Thing==3 then
+			if vars.PlayerBuff[t.PlayerIndex]["sleep"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Forced Sleep Immunity")
+			end 
+		elseif t.Thing==5 then
+			if vars.PlayerBuff[t.PlayerIndex]["insanity"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Insanity Immunity")
+			end 
+		elseif t.Thing==6 or t.Thing==7 or t.Thing==8 then
+			if vars.PlayerBuff[t.PlayerIndex]["poison"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Poison Immunity")
+			end 
+		elseif t.Thing==9 or t.Thing==10 or t.Thing==11 then
+			if vars.PlayerBuff[t.PlayerIndex]["disease"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Disease Immunity")
+			end 
+		elseif t.Thing==12 then
+			if vars.PlayerBuff[t.PlayerIndex]["paralysis"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Paralysis Immunity")
+			end 
+		elseif t.Thing==15 then
+			if vars.PlayerBuff[t.PlayerIndex]["stone"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Petrify Immunity")
+			end 
+		elseif t.Thing==23 then
+			if vars.PlayerBuff[t.PlayerIndex]["fear"]>Game.Time then
+				t.Allow=false
+				Game.ShowStatusText("Fear Immunity")
+			end 
+		end
+	end
+end
+
