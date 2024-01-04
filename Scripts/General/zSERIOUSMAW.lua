c2a9d1b5b89fc06e3cc9a8ad452c0cc1ae8d5b01
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-01-04 05:08:45 +0100

    Add files via upload

---
+++ Scripts/General/zSERIOUSMAW.lua
@@ -0,0 +1,19 @@
+function events.GameInitialized2()
+	name=Game.PlaceMonTxt
+	--write names in the following way
+	name[200]="Serpent Mother"
+	--200 is the NameId
+	--another example
+	name[201]="Captain Sharp-Tooth"
+	--201 is the NameId
+	name[202]="Captain Strong-Scale"
+	--202 is the NameId
+	name[203]="Captain Finch"
+	--203 is the NameId
+		name[204]="Mountain Man"
+	--204 is the NameId
+	name[304]="Huge Orc"
+	name[205]="Naga Empress"
+
+
+end
