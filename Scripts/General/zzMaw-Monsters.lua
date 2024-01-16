7370c5ec6f98d573e664f1ddeb69ad940c7f49b2
RawSugarX <114432644+RawSugarX@users.noreply.github.com>
2024-01-16 22:49:21 +0100

    Update zzMaw-Monsters.lua

--- Scripts/General/zzMaw-Monsters.lua
+++ Scripts/General/zzMaw-Monsters.lua
@@ -297,7 +297,7 @@ function events.LoadMap()
 			levelMult=Game.MonstersTxt[i].Level
 		end
 		
-		bonusDamage=math.max((levelMult-LevelB)*0.5,0)
+		bonusDamage=math.max(((levelMult-LevelB)*2)^0.88,0)
 		if bonusDamage>=20 then
 			levelMult=Game.MonstersTxt[i].Level
 		end
