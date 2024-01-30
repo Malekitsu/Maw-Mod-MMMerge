cadbc8cba4450f66a08b46fcc62f8f63067175ac
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-01-30 09:57:31 +0100

    Update zzMaw-Stats.lua

--- Scripts/General/zzMaw-Stats.lua
+++ Scripts/General/zzMaw-Stats.lua
@@ -331,7 +331,7 @@ function events.BuildStatInformationBox(t)
 					end
 				end
 			end
-			DPS1=math.round((dmg*(1+might/1000))*(1+(0.05+daggerCritBonus+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance)
+			DPS1=math.round((dmg*(1+might/1000))*(1+(0.05+daggerCritBonus+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance*damageMultiplier[Party[i]:GetIndex()]["Melee"])
 			
 			--RANGED
 			local low=Party[i]:GetRangedDamageMin()
@@ -346,7 +346,7 @@ function events.BuildStatInformationBox(t)
 			if m>=3 then
 				dmg=dmg*2
 			end
-			local DPS2=math.round((dmg*(1+might/1000))*(1+(0.05+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance)
+			local DPS2=math.round((dmg*(1+might/1000))*(1+(0.05+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance*damageMultiplier[Party[i]:GetIndex()]["Ranged"])
 			power=math.max(DPS1,DPS2)
 			
 			t.Text=string.format("%s\n\nPower: %s",t.Text,StrColor(255,0,0,power))
@@ -470,7 +470,7 @@ function events.BuildStatInformationBox(t)
 			end
 		end
 		--damage tracker
-		local DPS=math.round((dmg*(1+might/1000))*(1+(0.05+daggerCritBonus+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance)
+		local DPS=math.round((dmg*(1+might/1000))*(1+(0.05+daggerCritBonus+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance*damageMultiplier[Party[i]:GetIndex()]["Melee"])
 		t.Text=string.format("%s\n\nDamage per second: %s",t.Text,StrColor(255,255,100,DPS))
 		vars.damageTrack=vars.damageTrack or {}
 		vars.damageTrack[Party[i]:GetIndex()]=vars.damageTrack[Party[i]:GetIndex()] or 0
@@ -505,7 +505,7 @@ function events.BuildStatInformationBox(t)
 		local atk=Party[i]:GetRangedAttack()
 		local lvl=Party[i].LevelBase
 		local hitChance= (15+atk*2)/(30+atk*2+lvl)
-		local DPS=math.round((dmg*(1+might/1000))*(1+(0.05+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance)
+		local DPS=math.round((dmg*(1+might/1000))*(1+(0.05+0.01*luck/15)*(0.5+0.001*accuracy*3))/(delay/100)*hitChance*damageMultiplier[Party[i]:GetIndex()]["Ranged"])
 		local s,m=SplitSkill(Party[i].Skills[const.Skills.Bow])
 		if m>=3 then
 			DPS=DPS*2
