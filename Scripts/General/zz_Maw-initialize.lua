8d25c90a3b06cb6f1307908f86ed9e0f8ae78310
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-02-03 08:40:05 +0100

    Update zz_Maw-initialize.lua

--- Scripts/General/zz_Maw-initialize.lua
+++ Scripts/General/zz_Maw-initialize.lua
@@ -1,15 +1,10 @@
 --MAW SETTING HERE
---set true to homing projectiles for both players and monsters in real time combat
-RealTimeHoming=false
---reworked items with multiple enchants and increased damage/armor
-ItemRework=true
---reworked stats, with % increase to damage etc.
-StatsRework=true
+autoTargetHeals=true
 ColouredStats=true
 chargeKey=69
 healthPotionKey=71
 manaPotionKey=86
-
+Game.PatchOptions.FixMonstersBlockingShots=true
 ---------------------------------
 --HERE IS THE KEYBIND LIST--
 ---------------------------------
