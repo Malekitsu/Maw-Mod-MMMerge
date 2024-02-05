b8b852143d78ce7fc2ac8f8f9248c5c235b3be6c
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-02-05 20:14:16 +0100

    Update zzClasses.lua

--- Scripts/General/zzClasses.lua
+++ Scripts/General/zzClasses.lua
@@ -565,8 +565,8 @@ end
 function events.GameInitialized2()
 	function events.CalcDamageToMonster(t)
 		data=WhoHitMonster()
-		local pl=data.Player
 		if data and data.Player and (data.Player.Class==10 or data.Player.Class==11) then
+			local pl=data.Player
 			if data.Object==nil then
 				local breath = SplitSkill(data.Player:GetSkill(const.Skills.DragonAbility))
 				local fang, fangM = SplitSkill(data.Player:GetSkill(const.Skills.Unarmed))
