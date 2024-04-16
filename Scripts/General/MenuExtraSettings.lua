3209accf7a721ca4a50bb1961979aa6f7c3d1d39
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-04-16 16:06:41 +0200

    Update MenuExtraSettings.lua

--- Scripts/General/MenuExtraSettings.lua
+++ Scripts/General/MenuExtraSettings.lua
@@ -233,7 +233,7 @@ function events.GameInitialized2()
 	
 
 	-- Bolster amount
-	MAWBOLSTER={[0]="Easy", [50]="Nor\nmal", [100]="MAW", [150]="Hard", [200]="Hell", [250]="Night\nmare", [300]="Night\nmare"}
+	MAWBOLSTER={[0]="Easy", [50]="Norm.", [100]="MAW", [150]="Hard", [200]="Hell", [250]="Night\nmare", [300]="Night\nmare"}
 	Game.BolsterAmount = Game.BolsterAmount or 50
 	NumberRegulator(ExSetScr, 103, 220, "BolsterAmount",
 		function(t, val)
