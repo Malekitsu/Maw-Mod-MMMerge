2bad2b670c5d085c1a75335e8cc0b9e3f093614a
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-04-20 02:57:01 +0200

    Update zzMaw-Monsters.lua

--- Scripts/General/zzMaw-Monsters.lua
+++ Scripts/General/zzMaw-Monsters.lua
@@ -1749,7 +1749,7 @@ function events.AfterLoadMap()
 			end
 			for i=0,Map.Monsters.High do
 				local id=Map.Monsters[i].Id
-				if id%3==0 and Game.MonstersTxt[id].AIType~=1 then
+				if id%3==0 and Game.MonstersTxt[id].AIType~=0 then
 					table.insert(possibleMonsters,i)
 				end
 			end
