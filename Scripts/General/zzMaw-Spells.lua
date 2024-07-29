16c4926075d2ffff44eff5903306fa83cea8c646
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-07-29 18:04:49 +0200

    Update zzMaw-Spells.lua

--- Scripts/General/zzMaw-Spells.lua
+++ Scripts/General/zzMaw-Spells.lua
@@ -2767,7 +2767,11 @@ if buffRework then
 					elseif utilitySpell[buff] and utilitySpell[buff].PartyBuff then
 						local buffId=utilitySpell[buff].PartyBuff
 						--if buffId~=7 or (not Party.EnemyDetectorYellow and not Party.EnemyDetectorRed and Map.IndoorOrOutdoor==2) then --fly
-						Party.SpellBuffs[buffId].Caster=vars.mawbuff[buff]+1 --crashes otherwise
+						if type(vars.mawbuff[buff])=="string" then
+							Party.SpellBuffs[buffId].Caster=1 --crashes otherwise
+						else
+							Party.SpellBuffs[buffId].Caster=vars.mawbuff[buff]+1
+						end
 						Party.SpellBuffs[buffId].Bits=1 --allow fly
 						Party.SpellBuffs[buffId].ExpireTime=Game.Time+const.Week
 						--end
