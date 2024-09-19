4a174c07c433e5a4f43ea4d40b142242447f0064
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-09-19 21:41:57 +0200

    Update zzMaw-Maps.lua

--- Scripts/General/zzMaw-Maps.lua
+++ Scripts/General/zzMaw-Maps.lua
@@ -1791,9 +1791,9 @@ function calculateAffixPower(n, p)
 	elseif n == 4 then
 		power=5+p*0.5
 	elseif n == 5 then
-		power=10+p*0.2
+		power=7+p*0.1
 	elseif n == 6 then
-		power=10+p*0.2
+		power=7+p*0.1
 	elseif n == 7 then
 		power=1+p*0.1
 	elseif n == 8 then
