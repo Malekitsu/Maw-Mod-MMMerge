d649e877b333fe85af74ba518a7b04c5a28fea34
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-09-28 09:10:27 +0200

    Update zzClasses.lua

--- Scripts/General/zzClasses.lua
+++ Scripts/General/zzClasses.lua
@@ -1530,8 +1530,7 @@ function elementalistStacksDecay()
 			vars.eleTimer[id]=vars.eleTimer[id] or Game.Time
 			if Game.Time-vars.eleTimer[id]>const.Minute*2 then
 				vars.eleTimer[id]=Game.Time
-				vars.eleStacks[id]=0
-				--vars.eleStacks[id]=math.max(math.floor(vars.eleStacks[id]*0.9),0)
+				vars.eleStacks[id]=math.max(math.floor(vars.eleStacks[id]*0.5),0)
 			end
 		end	
 	end
