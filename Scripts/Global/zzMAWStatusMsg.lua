2288676bac51fe2089c714211644a6b6d129405c
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-09-28 20:13:15 +0200

    Update zzMAWStatusMsg.lua

--- Scripts/Global/zzMAWStatusMsg.lua
+++ Scripts/Global/zzMAWStatusMsg.lua
@@ -3,13 +3,13 @@ function events.CalcDamageToMonster(t)
 		local data=WhoHitMonster()
 		if data and data.Player then
 			local heal=t.Result^0.6
+			local fullHP=data.Player:GetFullHP()
 			if getMapAffixPower(32) then
 				heal=heal*(1-getMapAffixPower(32)/100)
 			end
 			if t.DamageKind==4 then
 				--melee
 				if gotVamp[data.Player:GetIndex()] and not data.Object then
-					local fullHP=t.Player:GetFullHP()
 					t.Player.HP=math.min(fullHP,t.Player.HP+heal)
 				end
 				--ranged
@@ -17,13 +17,11 @@ function events.CalcDamageToMonster(t)
 					if table.find(aoespells, data.Object.Spell) then
 						heal=heal/2
 					end
-					local fullHP=t.Player:GetFullHP()
 					t.Player.HP=math.min(fullHP,t.Player.HP+heal*0.5)
 				end
 			elseif data and data.Player then --spell leech
 				for i=1,2 do
 					it=t.Player:GetActiveItem(i)
-					local fullHP=t.Player:GetFullHP()
 					if it and it.Bonus2==40 then
 						t.Player.HP=math.min(fullHP,t.Player.HP+heal*0.5)
 					end
@@ -33,7 +31,6 @@ function events.CalcDamageToMonster(t)
 			local pl=data.Player
 			local race=Game.CharacterPortraits[pl.Face].Race
 			if race==const.Race.Vampire then
-				local fullHP=t.Player:GetFullHP()
 				local heal=heal/4
 				if pl.Class==40 or pl.Class==41 then
 					heal=heal*2
@@ -44,19 +41,38 @@ function events.CalcDamageToMonster(t)
 				heal=math.ceil(heal)
 				t.Player.HP=math.min(fullHP,t.Player.HP+heal)
 			end
-			if buffRework and vars.mawbuff[91] then --vampiric weapon spell
-				if getBuffSkill(91)>0 then
-					heal=math.ceil(heal/2)
-					t.Player.HP=math.min(fullHP,t.Player.HP+heal)
+			if buffRework and getBuffSkill(91)>0 then --vampiric aura buff
+				local heal=t.Result^0.6*0.5
+				if getMapAffixPower(32) then
+					heal=heal*(1-getMapAffixPower(32)/100)
 				end
+				if t.DamageKind==4 then
+					--melee
+					if not data.Object then
+						t.Player.HP=math.min(fullHP,t.Player.HP+heal)
+					end
+					--ranged
+					if data.Object and data.Object.Spell==133 then
+						if table.find(aoespells, data.Object.Spell) then
+							heal=heal/2
+						end
+						t.Player.HP=math.min(fullHP,t.Player.HP+heal*0.5)
+					end
+				elseif data and data.Player then --spell leech
+					t.Player.HP=math.min(fullHP,t.Player.HP+heal*0.5)
+				end 
 			end
+			
+			
 			local fireAuraDamage=0
 			local fireRes=t.Monster.Resistances[0]%1000
 			if data and not data.Object and t.DamageKind==4 then
 				for i=0,1 do
 					local it=t.Player:GetActiveItem(i)
 					local damage=calcFireAuraDamage(pl, it, fireRes)
-					fireAuraDamage=fireAuraDamage+damage
+					if damage then
+						fireAuraDamage=fireAuraDamage+damage
+					end
 				end
 			elseif data.Object and data.Spell<133 and data.Spell>0 then --spells
 				for i=0,2 do
@@ -69,11 +85,10 @@ function events.CalcDamageToMonster(t)
 			elseif data and data.Object and (data.Object.Spell==133 or data.Spell==135) then --bow/blasters
 				local it=t.Player:GetActiveItem(2)
 				local damage=calcFireAuraDamage(pl, it, fireRes)
-				if damage>fireAuraDamage then
+				if damage and damage>fireAuraDamage then
 					fireAuraDamage=damage
 				end
 			end
-			debug.Message(fireAuraDamage)
 			t.Result=t.Result+fireAuraDamage
 		end
 	end
