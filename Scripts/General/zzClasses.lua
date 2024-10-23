b3785a55b4e043ee822d3a36a064b62bdb26e9e8
RawSugarX <114432644+RawSugarX@users.noreply.github.com>
2024-10-23 13:53:05 +0200

    Update zzClasses.lua

--- Scripts/General/zzClasses.lua
+++ Scripts/General/zzClasses.lua
@@ -939,7 +939,7 @@ local function shamanSkills(isShaman, id)
 		local waterReduction=math.round(m3^2.25/(100+pl.LevelBase)*50*mult)
 		txt=baseSchoolsTxt[14] .. "\n\nReduce all damage taken by " .. waterReduction .. "(calculated after resistances)"
 		Skillz.setDesc(14,1,txt)
-		local armsmasterDamage=math.round(m5^2.4/(10+pl.LevelBase)*10)
+		local armsmasterDamage=math.round(m4^2.5/(10+pl.LevelBase)*10)
 		txt=baseSchoolsTxt[15] .. "\n\nIncreases melee damage by ".. armsmasterDamage .. ""
 		Skillz.setDesc(15,1,txt)
 		local spelldh=math.round(m5^2/pl.LevelBase/6*100)
