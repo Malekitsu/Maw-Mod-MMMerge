307545f3b249d99a6be594cbbe8b3e04ceb644f7
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-11-04 17:38:00 +0100

    Update zzAlchemy.lua

--- Scripts/General/zzAlchemy.lua
+++ Scripts/General/zzAlchemy.lua
@@ -1037,7 +1037,7 @@ function events.MonsterKilled(mon)
 	mon.Ally=9999
 	
 	if getMapAffixPower(9) and math.random()<getMapAffixPower(9)/100 then
-		pseudoSpawnpoint{monster = mon.Id,  x = mon.X, y = mon.Y, z = mon.Z, count = 1, powerChances = {55, 30, 15}, radius = 128, group = 2,transform = function(mon) mon.Ally = 0, mon.Hostile=true, mon.ShowAsHostile=true end}
+		pseudoSpawnpoint{monster = mon.Id,  x = mon.X, y = mon.Y, z = mon.Z, count = 1, powerChances = {55, 30, 15}, radius = 128, group = 2,transform = function(mon) mon.Ally = 0 mon.Hostile=true mon.ShowAsHostile=true end}
 	end
 	
 	--level bonus
