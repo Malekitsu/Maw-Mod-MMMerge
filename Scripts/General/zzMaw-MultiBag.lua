ff4a2e2eba2bccd5cf55c5ab554011cb1210f51a
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2024-11-27 02:58:12 +0100

    Update zzMaw-MultiBag.lua

--- Scripts/General/zzMaw-MultiBag.lua
+++ Scripts/General/zzMaw-MultiBag.lua
@@ -108,7 +108,7 @@ function events.Action(t)
 				else
 					multibagButton[i+5].IUpSrc="SlChar" .. i .. "U"
 				end
-				if Party.High>0 then
+				if Party.High>0 and Game.CurrentScreen==7 then
 					multibagButton[i+5].Active=false
 				else
 					multibagButton[i+5].Active=true
