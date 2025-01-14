af200b82fc539947ee27ff7f14853d3b779ff16e
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-01-14 17:43:22 +0100

    Update zzMaw-Stats.lua

--- Scripts/General/zzMaw-Stats.lua
+++ Scripts/General/zzMaw-Stats.lua
@@ -912,8 +912,8 @@ function events.CalcDamageToMonster(t)
 		end
 	end
 	if t.Player and vars.legendaries and vars.legendaries[t.PlayerIndex] and table.find(vars.legendaries[t.PlayerIndex], 29) then
-		if data and data.Object==nil and t.DamageKind~=4 then return end --disable for melee elemental damage
-		if data and table.find(aoespells, data.Spell) and math.random()>0.4 then return end
+		if data and data.Object==nil and t.DamageKind~=4 then goto continue end --disable for melee elemental damage
+		if data and table.find(aoespells, data.Spell) and math.random()>0.4 then goto continue end
 		for i=0, 10 do
 			if i~=5 then
 				if i==4 then
@@ -929,6 +929,7 @@ function events.CalcDamageToMonster(t)
 			end
 		end
 	end
+	::continue::
 	--retaliation code
 	if t.Player then
 		local id=t.Player:GetIndex()
