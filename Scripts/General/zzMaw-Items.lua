1014197e196e8b87c671cc27df5d7c652e6e259d
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-04-08 22:29:36 +0200

    Update zzMaw-Items.lua

--- Scripts/General/zzMaw-Items.lua
+++ Scripts/General/zzMaw-Items.lua
@@ -145,6 +145,14 @@ function events.BeforeLoadMap()
 		enc1Chance = {20, 20, 21, 21, 22, 22, 23, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29}
 		enc2Chance = {10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18, 19, 19}
 		spcEncChance = {40, 40, 41, 41, 42, 42, 43, 43, 44, 44, 45, 45, 46, 46, 47, 47, 48, 48, 49, 49}
+	elseif higherLootPowerRange then
+		encStrDown={5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,210,220}
+		encStrUp={5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,180,185,190,195,200,210,220}
+
+
+		enc1Chance={20,30,40,50,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80}
+		enc2Chance={20,30,35,40,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60}
+		spcEncChance={5,10,15,20,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40}
 	else
 		encStrDown={2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84}
 		encStrUp={3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99,102,105,108,111,114,117,120,125,130}
@@ -760,6 +768,21 @@ function events.ItemGenerated(t)
 					Game.ShowStatusText("You found " .. itemGold+goldGained .. " gold! (" .. tierList[itemPower] .. " " .. Game.ItemsTxt[itemID].NotIdentifiedName .. " filtered)")
 				end
 			end
+		end
+		if higherLootPowerRange then
+			local minValue=0
+			local itemType=t.Item.BonusExpireTime
+			if itemType==1 then
+				minValue=0.3
+			elseif itemType==2 then
+				minValue=0.3
+			elseif itemType>=10 and itemType<100 then
+				minValue=0.3
+			end
+			
+			t.Item.BonusStrength=math.random(1+t.Item.BonusStrength*minValue,t.Item.BonusStrength)
+			t.Item.MaxCharges=math.min(math.random(1+t.Item.MaxCharges*minValue,t.Item.MaxCharges*1.5),255)
+			t.Item.Charges=t.Item.Charges-t.Item.Charges%1000+math.random(1+t.Item.Charges%1000*minValue,t.Item.Charges%1000)
 		end
 	end
 end
