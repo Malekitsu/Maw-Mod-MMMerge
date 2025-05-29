a270807f0eebe210f48338bdfe2113721c7e26c4
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-05-29 15:17:35 +0200

    Update zzClasses.lua

--- Scripts/General/zzClasses.lua
+++ Scripts/General/zzClasses.lua
@@ -569,11 +569,11 @@ function events.GameInitialized2()
 	function events.GetAttackDelay(t)
 		if Game.CharacterPortraits[t.Player.Face].Race==const.Race.Dragon then
 			if useBreathCooldown or t.Ranged then
-				local s, m = SplitSkill(pl:GetSkill(const.Skills.DragonAbility))
+				local s, m = SplitSkill(t.Player:GetSkill(const.Skills.DragonAbility))
 				t.Result=t.Result * (1+0.03*s)
 				useBreathCooldown=false
 			else
-				local s, m = SplitSkill(pl:GetSkill(const.Skills.Unarmed))
+				local s, m = SplitSkill(t.Player:GetSkill(const.Skills.Unarmed))
 				t.Result=t.Result * (1+0.03*s)
 			end
 		end	
