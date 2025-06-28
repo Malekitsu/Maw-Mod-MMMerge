7285c2f6b6db443a96538d53d8699108c66d0e58
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-06-28 20:13:05 +0200

    Update zzMAW-Skills.lua

--- Scripts/General/zzMAW-Skills.lua
+++ Scripts/General/zzMAW-Skills.lua
@@ -1267,7 +1267,7 @@ function MawRegen()
 				local s,m=SplitSkill(pl:GetSkill(const.Skills.Water))
 				regenSP[i]=regenSP[i]+ (0.6+m*0.2) * timeMultiplier
 			end
-			pl.SP = math.min(vars.currentManaPool[i], pl.SP + math.floor(regenSP[i]))
+			pl.SP = math.min(FSP, pl.SP + math.floor(regenSP[i]))
 			regenSP[i]=regenSP[i]%1
 			lastSP[i]=pl.SP
 		end
