b8fd6340ff3b1e77cfbed922f370a5346de7a9e1
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-07-12 13:56:09 +0200

    Update zzMaw-Spells.lua

--- Scripts/General/zzMaw-Spells.lua
+++ Scripts/General/zzMaw-Spells.lua
@@ -1681,13 +1681,10 @@ function events.Action(t)
 end
 
 function events.Tick()
-	lastAscension=lastAscension or -1
-	lastCheckSkill=lastCheckSkill or -1
-	local playerToAscend=-1
-	local lowestDelay=math.huge
-	playerToAscend=Game.CurrentPlayer
-	if playerToAscend>Party.High then return end
 	if Game.CurrentPlayer==-1 then
+		lastAscension=lastAscension or -1
+		local playerToAscend=-1
+		local lowestDelay=math.huge
 		for i=0,Party.High do
 			local pl=Party[i]
 			local delay=pl.RecoveryDelay
@@ -1696,13 +1693,10 @@ function events.Tick()
 				playerToAscend=i
 			end
 		end
-	end
-	if playerToAscend~=lastAscension then
-		ascension(playerToAscend)
-		lastAscension=playerToAscend
-	end
-	if playerToAscend~=lastCheckSkill then
-		checkSkills(playerToAscend)
+		if playerToAscend~=lastAscension then
+			ascension(playerToAscend)
+			lastAscension=playerToAscend
+		end
 	end
 end
 
@@ -1839,12 +1833,12 @@ function ascension(customIndex)
 		--Healing Spells
 		-----------------------
 		healingSpells={
-			[const.Spells.RemoveCurse]=    {["Cost"]={0,5,10,20,[0]=0}, ["Base"]={0,10,20,30,[0]=0}, ["Scaling"]={0,4,6,8}},
+			[const.Spells.RemoveCurse]=    {["Cost"]={0,5,10,20,[0]=0}, ["Base"]={0,10,15,25,[0]=0}, ["Scaling"]={0,3,4,5}},
 			[const.Spells.SharedLife]=    {["Cost"]={0,0,25,40,[0]=0}, ["Base"]={0,0,0,0,[0]=0}, ["Scaling"]={0,0,7,9}},
-            [const.Spells.Resurrection]={["Cost"]={0,0,0,100,[0]=0}, ["Base"]={0,0,0,150,[0]=0}, ["Scaling"]={0,0,0,21}},
-            [const.Spells.Heal]=        {["Cost"]={2,4,6,8,[0]=0}, ["Base"]={4,8,12,16,[0]=0}, ["Scaling"]={2,3,4,6}},
-            [const.Spells.CureDisease]=    {["Cost"]={0,0,15,25,[0]=0}, ["Base"]={0,0,25,40,[0]=0}, ["Scaling"]={0,0,7,10}},
-            [const.Spells.PowerCure]=    {["Cost"]={0,0,0,30,[0]=0}, ["Base"]={0,0,0,15,[0]=0}, ["Scaling"]={0,0,0,4}}
+            [const.Spells.Resurrection]={["Cost"]={0,0,0,100,[0]=0}, ["Base"]={0,0,0,100,[0]=0}, ["Scaling"]={0,0,0,14}},
+            [const.Spells.Heal]=        {["Cost"]={2,4,6,8,[0]=0}, ["Base"]={4,6,8,10,[0]=0}, ["Scaling"]={1,2,3,4}},
+            [const.Spells.CureDisease]=    {["Cost"]={0,0,15,25,[0]=0}, ["Base"]={0,0,15,25,[0]=0}, ["Scaling"]={0,0,5,7}},
+            [const.Spells.PowerCure]=    {["Cost"]={0,0,0,30,[0]=0}, ["Base"]={0,0,0,10,[0]=0}, ["Scaling"]={0,0,0,3}}
 		}
 		for i=1, 6 do
 			local ascensionLevel=getAscensionTier(s,healingList[i])
