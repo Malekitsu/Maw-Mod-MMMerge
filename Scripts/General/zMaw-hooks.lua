93602715c0725a0be66d2fc03afc00435574f060
R. Andrew Burza <andrew.ohana@gmail.com>
2025-08-31 19:16:06 -0700

    cleanup hook scoping

--- Scripts/General/zMaw-hooks.lua
+++ Scripts/General/zMaw-hooks.lua
@@ -1,27 +1,25 @@
-local u1, u2, u4, i1, i2, i4 = mem.u1, mem.u2, mem.u4, mem.i1, mem.i2, mem.i4
-local hook, autohook, autohook2, asmpatch = mem.hook, mem.autohook, mem.autohook2, mem.asmpatch
-local max, min, round, random = math.max, math.min, math.round, math.random
-local format = string.format
 
-local function getSFTItem(p)
-	local i = (p - Game.SFTBin.Frames["?ptr"]) / Game.SFTBin.Frames[0]["?size"]
-	return Game.SFTBin.Frames[i]
-end
-
--- cosmetic change: some monsters (mainly bosses) can be larger
-local scaleHook = function(indoor)
-	return function(d)
-		local t = {Scale = d.eax, Frame = getSFTItem(d.ebx)}
-		t.MonsterIndex, t.Monster = internal.GetMonster(indoor and d.edi or (d.edi - 0x9A))
-		events.call("MonsterSpriteScale", t)
-		d.eax = t.Scale
+do
+	local function getSFTItem(p)
+		local i = (p - Game.SFTBin.Frames["?ptr"]) / Game.SFTBin.Frames[0]["?size"]
+		return Game.SFTBin.Frames[i]
 	end
+
+	-- cosmetic change: some monsters (mainly bosses) can be larger
+	local scaleHook = function(indoor)
+		return function(d)
+			local t = {Scale = d.eax, Frame = getSFTItem(d.ebx)}
+			t.MonsterIndex, t.Monster = internal.GetMonster(indoor and d.edi or (d.edi - 0x9A))
+			events.call("MonsterSpriteScale", t)
+			d.eax = t.Scale
+		end
+	end
+
+	-- outdoor
+	mem.autohook2(0x47AC26, scaleHook())
+	mem.autohook2(0x47AC46, scaleHook())
+
+	-- indoor
+	mem.autohook2(0x43D02E, scaleHook(true))
+	mem.autohook2(0x43D04D, scaleHook(true))
 end
-
--- outdoor
-autohook2(0x47AC26, scaleHook())
-autohook2(0x47AC46, scaleHook())
-
--- indoor
-autohook2(0x43D02E, scaleHook(true))
-autohook2(0x43D04D, scaleHook(true))
