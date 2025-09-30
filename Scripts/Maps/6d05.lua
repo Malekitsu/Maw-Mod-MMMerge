ed55fe24748605fb52e600b14c80048ead9c006b
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-09-30 14:48:16 +0900

    Update 6d05.lua

--- Scripts/Maps/6d05.lua
+++ Scripts/Maps/6d05.lua
@@ -4,7 +4,6 @@ evt.hint[9] = evt.str[2]  -- "Door"
 evt.map[9] = function()
 	if evt.Cmp{"Inventory", Value = 2108} then         -- "Key to Snergle's Chambers"
 		evt.SetDoorState{Id = 9, State = 1}
-		evt.Subtract{"Inventory", Value = 2108}         -- "Key to Snergle's Chambers"
 		evt.SummonMonsters{TypeIndexInMapStats = 1, Level = 2, Count = 3, X = 387, Y = 8721, Z = 257, NPCGroup = 0, unk = 0}
 		evt.SummonMonsters{TypeIndexInMapStats = 1, Level = 3, Count = 3, X = 420, Y = 8538, Z = 257, NPCGroup = 0, unk = 0}
 		evt.SummonMonsters{TypeIndexInMapStats = 1, Level = 2, Count = 3, X = 44, Y = 8517, Z = 247, NPCGroup = 0, unk = 0}
@@ -15,4 +14,5 @@ evt.map[9] = function()
 		evt.StatusText{Str = 28}         -- "The door is locked"
 	end
 end
+
 
