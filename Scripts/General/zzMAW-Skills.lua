a11482f7ec4043929b63504b2013e835673aaee2
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-10-16 11:17:23 +0900

    Update zzMAW-Skills.lua

--- Scripts/General/zzMAW-Skills.lua
+++ Scripts/General/zzMAW-Skills.lua
@@ -86,11 +86,11 @@ skillDamage =
 
 skillAC =
 {
-	[const.Skills.Staff]	= {[0]=0, 2, 3, 3, 4,},
+	[const.Skills.Staff]	= {[0]=0, 0, 2, 3, 4,},
 	[const.Skills.Sword]	= {[0]=0, 0, 0, 1, 2,},
 	[const.Skills.Dagger]	= {[0]=0, 0, 0, 0, 0,},
 	[const.Skills.Axe]		= {[0]=0, 0, 0, 0, 0,},
-	[const.Skills.Spear]	= {[0]=0, 4, 6, 8, 10,},
+	[const.Skills.Spear]	= {[0]=0, 0, 4, 6, 8,},
 	[const.Skills.Bow]		= {[0]=0, 0, 0, 0, 0,},
 	[const.Skills.Mace]		= {[0]=0, 0, 0, 0, 0,},
 	[const.Skills.Blaster]	= {[0]=0, 0, 0, 0, 0,},
@@ -104,7 +104,7 @@ skillAC =
 }
 skillResistance =
 {
-	[const.Skills.Staff]	= {[0]=0, 2, 2, 3, 4},
+	[const.Skills.Staff]	= {[0]=0, 0, 2, 3, 4},
 	[const.Skills.Sword]	= {[0]=0, 0, 0, 0, 0},
 	[const.Skills.Dagger]	= {[0]=0, 0, 0, 0, 0},
 	[const.Skills.Axe]		= {[0]=0, 0, 0, 0, 0},
