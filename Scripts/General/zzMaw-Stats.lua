23724c7235edc7e2d89fd1d68149ad98ad1c3df1
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2025-12-28 14:27:45 +0100

    Update zzMaw-Stats.lua

--- Scripts/General/zzMaw-Stats.lua
+++ Scripts/General/zzMaw-Stats.lua
@@ -265,11 +265,17 @@ function getSpellDelay(pl,spell)
 	local hasteDiv=1
 	if vars.MAWSETTINGS.buffRework=="ON" then
 		local hasteMult=1
-		if Party.SpellBuffs[8].ExpireTime>=Game.Time then
+		if Party.SpellBuffs[8].ExpireTime>=Game.Time or pl.SpellBuffs[const.PlayerBuff.Haste].ExpireTime>Game.Time then
 			local s, m=getBuffSkill(5)
 			local s2,m2=getBuffSkill(86)
-			s=math.max(s,s2/1.5)
-			m=math.max(m,m2)
+			local s3=0
+			local m3=0
+			if pl.SpellBuffs[const.PlayerBuff.Haste].ExpireTime>Game.Time then
+				s3=25
+				m3=3
+			end
+			s=math.max(s,s2/1.5,s3)
+			m=math.max(m,m2,m3)
 			hasteDiv=math.max(1+buffPower[5].Base[m]/100+buffPower[5].Scaling[m]*s/1000, hasteDiv)
 		end
 	end
